12bc5a650cc533cc5156dcca2957788e51db28e4
KatlegoJ <77067592+KatlegoJ@users.noreply.github.com>
2021-03-24 16:04:40 +0200

    Update task 0.4.py

--- task 0.4.py
+++ task 0.4.py
@@ -3,6 +3,7 @@ def even_or_odd(num):
     if num % 2 == 0:
         return "even"
     elif num % 2 !=0:
-        return "odd"
-    even_or_odd(17)
+        print( "odd")
+        
+even_or_odd(17)    
     

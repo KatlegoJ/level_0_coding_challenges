48c4736559e1ae5bbbb730a75fcdbdd7cb180284
KatlegoJ <77067592+KatlegoJ@users.noreply.github.com>
2021-03-24 16:06:04 +0200

    Update task 0.5.py

--- task 0.5.py
+++ task 0.5.py
@@ -1,10 +1,10 @@
 def triangle_area(a,b,c):  
           
     # calculate the semi-perimeter  
-    semi = (a + b + c) / 2
+    semi_perimeter = (a + b + c) / 2
       
     # calculate the area  
-    area = (semi * (semi - a) * (semi - b) * (semi - c)) ** 0.5
+    area = (semi_perimeter * (semi_perimeter - a) * (semi_perimeter - b) * (semi_perimeter - c)) ** 0.5
     
     return area
- 
+triangle_area(5, 6, 7)

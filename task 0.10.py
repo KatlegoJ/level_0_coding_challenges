8b409fca8be655f0ccf6c88ca0dff2e926753211
KatlegoJ <77067592+KatlegoJ@users.noreply.github.com>
2021-03-24 16:13:25 +0200

    Update task 0.10.py

--- task 0.10.py
+++ task 0.10.py
@@ -2,4 +2,7 @@ def letters (word1, word2):
     alphabet = 'abcdefghijklmnopqrstuvwxyz, ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     for letter in alphabet :
         if letter in word1 and letter in word2:
-            print (letter)
+
+            print (letter,",",end = " ")
+            
+letters('Enelo','Bonolo')
